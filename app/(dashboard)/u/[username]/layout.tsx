65efbc32ae69f6db1e094ed1293bcdb25c0c8517
Jess Roque <68854057+jessdolt@users.noreply.github.com>
2024-01-18 17:52:25 +0800

    creator dashboard design init

---
+++ app/(dashboard)/u/[username]/layout.tsx
@@ -0,0 +1,33 @@
+import { getSelfByUsername } from "@/lib/auth-service"
+import { redirect } from "next/navigation"
+
+import { Navbar } from "./_components/navbar"
+import { Sidebar } from "./_components/sidebar"
+
+interface CreatorLayoutProps {
+  params: { username: string }
+  children: React.ReactNode
+}
+
+const CreatorPageLayout: React.FC<CreatorLayoutProps> = async ({
+  children,
+  params: { username },
+}) => {
+  const self = await getSelfByUsername(username)
+
+  if (!self) {
+    redirect("/")
+  }
+
+  return (
+    <>
+      <Navbar />
+      <div className="flex h-full pt-20">
+        <Sidebar />
+        {children}
+      </div>
+    </>
+  )
+}
+
+export default CreatorPageLayout
